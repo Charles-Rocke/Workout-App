f0627a7c8caa3c1bdf82171bb7c4ea84d563fc16
SpOOk <spook.devv@gmail.com>
2023-07-28 14:38:45 -0400

    Initial commit for workout app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+if (typeof window !== "undefined") {
+  window.React = React;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {window.React === React ? <App /> : <h1>Not a React App</h1>}
+  </React.StrictMode>
+);
